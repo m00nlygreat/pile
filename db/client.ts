c5b35c903e88eca6d6b6d893827c35f275779b9c
공대적 문과생 <moonlygreat@gmail.com>
2025-09-18 17:24:07 +0900

    Run Drizzle migrations when initializing SQLite client

--- db/client.ts
+++ db/client.ts
@@ -3,6 +3,7 @@ import path from "node:path";
 
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 
 import * as schema from "./schema";
 
@@ -14,6 +15,18 @@ const databasePath = path.join(dataDirectory, "pile.db");
 
 const sqlite = new Database(databasePath);
 
-export const db = drizzle(sqlite, { schema });
+const dbInstance = drizzle(sqlite, { schema });
+
+const migrationsFolder = path.join(process.cwd(), "drizzle");
+const migrationState = globalThis as {
+  __pileDbMigrated?: boolean;
+};
+
+if (!migrationState.__pileDbMigrated) {
+  migrate(dbInstance, { migrationsFolder });
+  migrationState.__pileDbMigrated = true;
+}
+
+export const db = dbInstance;
 
 export type DatabaseInstance = typeof db;
